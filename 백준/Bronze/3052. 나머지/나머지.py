2ca379f88d5280ad2042f37c4eac63a38de15bf0
Hyeongi Kim <61077215+hgkim00@users.noreply.github.com>
2024-01-16 20:34:40 +0900

    [Bronze II] Title: 나머지, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+answer = []
+for _ in range(10) :
+  num = int(input())
+  result = num % 42
+  answer.append(result)
+
+answer = set(answer)
+print(len(answer))
